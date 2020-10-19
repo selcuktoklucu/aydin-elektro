92f0151ac82d6821709833dc5b90a4d3d093aeb2
pozithron <36094301+pozithron@users.noreply.github.com>
2020-10-19 02:07:47 -0400

    Updates

---
+++ precache-manifest.6214df82d615233e442e8e100d7227fd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0a5ba4bf2cb725b615c8d2b0287aaa2",
+    "url": "/aydin-elektro/index.html"
+  },
+  {
+    "revision": "8153b760ae9eaf1a5956",
+    "url": "/aydin-elektro/static/css/2.274f2f50.chunk.css"
+  },
+  {
+    "revision": "bb2b62ac747fbdcc508f",
+    "url": "/aydin-elektro/static/css/main.b64d91cb.chunk.css"
+  },
+  {
+    "revision": "8153b760ae9eaf1a5956",
+    "url": "/aydin-elektro/static/js/2.59675e16.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/aydin-elektro/static/js/2.59675e16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2b62ac747fbdcc508f",
+    "url": "/aydin-elektro/static/js/main.aa6cd689.chunk.js"
+  },
+  {
+    "revision": "98a207030a0dfbf44f42",
+    "url": "/aydin-elektro/static/js/runtime-main.68a85646.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/aydin-elektro/static/media/logo.ee7cd8ed.svg"
+  }
+]);
